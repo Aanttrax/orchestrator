ca9501201dbd03575e64e3b71d2247a7c8fb868a
Aanttrax <arielrt20@gmail.com>
2024-12-22 16:06:57 -0500

    feat: :sparkles: implementation of headers

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -5,15 +5,17 @@ import TaskService from '@services/task.service';
 export default class TaskController {
   static async createTask(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      await TaskService.createTask(req.body);
+      const token = req.headers?.['auth-token'];
+      await TaskService.createTask(req.body, token as string);
       res.status(201).json({ success: true, response: 'Task created' });
     } catch (e: unknown) {
       next(e);
     }
   }
-  static async getTasks(_req: Request, res: Response, next: NextFunction): Promise<void> {
+  static async getTasks(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const tasks: Task[] = await TaskService.getTasks();
+      const token = req.headers?.['auth-token'];
+      const tasks: Task[] = await TaskService.getTasks(token as string);
       res.status(200).json({ success: true, response: tasks });
     } catch (e: unknown) {
       next(e);
@@ -30,8 +32,9 @@ export default class TaskController {
   }
   static async updateTask(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
+      const token = req.headers?.['auth-token'];
       const { taskId } = req.params;
-      await TaskService.updateTask(taskId, req.body);
+      await TaskService.updateTask(taskId, req.body, token as string);
       res.status(200).json({ success: true, response: 'Task Updated' });
     } catch (e: unknown) {
       next(e);
